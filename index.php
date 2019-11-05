e4b5b6f85c2ed11f57793af37a6bd946b5f7eca7
KMK <kirillmk_kmk@mail.ru>
2019-11-05 11:06:35 +0700

    removed the unused variable

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $main_content = include_template("main.php", [
     "cats" => $cats,
     "lots" => $lots
 ]);
-print $layout_content = include_template("layout.php", [
+print include_template("layout.php", [
     "main_content" => $main_content,
     "title" => "Главная",
     "is_auth" => rand(0, 1),

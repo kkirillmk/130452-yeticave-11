1a8a1980458304d8db945351fe4c036e0a7cec95
KMK <kirillmk_kmk@mail.ru>
2019-11-13 09:51:55 +0700

    Formated php with sql code

--- index.php
+++ index.php
@@ -9,27 +9,27 @@ $cats = [];
 
 if (!$sql_connect) {
     echo ("Ошибка подключения: " . mysqli_connect_error());
-} else {
-    $sql = "SELECT l.`name`, `starting_price`, `img`, MAX(b.`bet_sum`) AS `current_price`,
-            c.`name`  AS `category`, `date_end` FROM lots l
-            LEFT JOIN `bets` b ON b.`id_lot` = l.`id`
-            JOIN `categories` c ON c.`id` = l.`id_category`
-            WHERE l.`date_end` > NOW()
-            GROUP BY l.`id` ORDER BY l.`id` DESC";
-    $result = mysqli_query($sql_connect, $sql);
-    if (!$result) {
-        echo ("Ошибка запроса: " . mysqli_error($sql_connect));
-    } else {
-        $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }
+    exit;
 }
+$sql = "SELECT l.`name`, `starting_price`, `img`, MAX(b.`bet_sum`) AS `current_price`,
+        c.`name`  AS `category`, `date_end` FROM lots l
+        LEFT JOIN `bets` b ON b.`id_lot` = l.`id`
+        JOIN `categories` c ON c.`id` = l.`id_category`
+        WHERE l.`date_end` > NOW()
+        GROUP BY l.`id` ORDER BY l.`id` DESC";
+$result = mysqli_query($sql_connect, $sql);
+if (!$result) {
+    echo ("Ошибка запроса: " . mysqli_error($sql_connect));
+    exit;
+}
+$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
 $sql = "SELECT * FROM `categories`";
 $result = mysqli_query($sql_connect, $sql);
 if (!$result) {
     echo ("Ошибка запроса: " . mysqli_error($sql_connect));
-} else {
-    $cats = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    exit;
 }
+$cats = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 $main_content = include_template("main.php", [
     "cats" => $cats,

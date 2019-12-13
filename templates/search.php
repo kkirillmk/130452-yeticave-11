4643459b14acae3c93d1e06bb68ccb9df6dfe63b
KMK <kirillmk_kmk@mail.ru>
2019-12-13 20:47:25 +0700

    Started to add page categories

--- templates/search.php
+++ templates/search.php
@@ -69,14 +69,6 @@
                 <?php endforeach; ?>
             </ul>
         </section>
-        <ul class="pagination-list">
-            <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
-            <li class="pagination-item pagination-item-active"><a>1</a></li>
-            <li class="pagination-item"><a href="#">2</a></li>
-            <li class="pagination-item"><a href="#">3</a></li>
-            <li class="pagination-item"><a href="#">4</a></li>
-            <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
-        </ul>
     <?php else: ?>
         <h2>Ничего не найдено по вашему запросу</h2>
     <?php endif; ?>
